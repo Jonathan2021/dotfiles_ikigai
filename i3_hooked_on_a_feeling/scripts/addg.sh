4708638afbf74962e27ab9a1414b83d6840e59fd
yedhink <yedhin1998@gmail.com>
2018-05-23 18:40:08 +0530

    23rd May : Create new scripts

--- i3_hooked_on_a_feeling/scripts/addg.sh
+++ i3_hooked_on_a_feeling/scripts/addg.sh
@@ -2,5 +2,5 @@
 while read -r line;do
 	git add "$line"
 	git commit -m "23rd May : Create new scripts" 
-done < <(git status --porcelain | awk '{print }' | awk -F'/' '{print $NF}')
+done < <(git status "$1" --porcelain | awk '{print }' | awk -F'/' '{print $NF}')
 echo "worked!!!!"
