8cec939cf56a778fa1cc952ebfe8df21bfc51f27
yedhink <yedhin1998@gmail.com>
2018-05-25 07:02:06 +0530

    23rd May : Create new scripts

--- scripts/blah/tmco.sh
+++ scripts/blah/tmco.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-bgco=`xrdb -query | grep background | tail -n 1 | awk '{print $2}'`
-tmux set -g status-bg "$bgco"
-tmux set -g pane-border-fg "$bgco"
-tmux set -g pane-active-border-fg "$bgco"
-tmux set -g pane-active-border-bg "$bgco"
+bgco=`xrdb -query | grep foreground | tail -n 1 | awk '{print $2}'`
+tmux set -g status-bg "#f7f7f9"
+tmux set -g pane-border-fg "#f7f7f9"
+tmux set -g pane-active-border-fg "#f7f7f9"
+tmux set -g pane-active-border-bg "#f7f7f9"
