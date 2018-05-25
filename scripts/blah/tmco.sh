9f139b4158e40f23fb975289e1716043c91f8568
yedhink <yedhin1998@gmail.com>
2018-05-25 17:46:36 +0530

    25 May 18 : Create/Update new files

--- scripts/blah/tmco.sh
+++ scripts/blah/tmco.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/bin/bashi
+#pkill lemonbar || :
 bgco=`xrdb -query | grep background | tail -n 1 | awk '{print $2}'`
+fgco=`xrdb -query | grep foreground | tail -n 1 | awk '{print $2}'`
+
+if [ $(cat ~/curtheme) == "dark" ]; then
+	tmux set -g status-fg "#19FF19"
+else
+	tmux set -g status-fg "$fgco"
+fi
+
 tmux set -g status-bg "$bgco"
 tmux set -g pane-border-fg "$bgco"
 tmux set -g pane-active-border-fg "$bgco"
