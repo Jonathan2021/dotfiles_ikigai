94dd851b96bdd5f183a43effca9c04efacd3157f
yedhink <yedhin1998@gmail.com>
2018-05-28 15:23:52 +0530

    25th May 2018 : Updated

--- scripts/blah/addg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while read -r line;do
-	git add "$line"
-	git commit -m "23rd May : Create new scripts" 
-done < <(git status "$1" --porcelain | awk '{print }' | awk -F'/' '{print $NF}')
-echo "worked!!!!"
